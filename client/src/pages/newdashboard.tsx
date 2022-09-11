b8b0dc73e3d849de11451f73de4f4f09e5c0025d
Edison Hua <iseahound@gmail.com>
2022-09-11 00:08:50 -0400

    Some fixes :)

--- client/src/pages/newdashboard.tsx
+++ client/src/pages/newdashboard.tsx
@@ -69,7 +69,7 @@ const Sidebar = (props: any) => {
         <nav onClick={() => handleLogout()}>
           <div className="dashSidebarCardIcon">
             <svg className="ml-2 h-8 w-8 text-[#D6B8B6]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"/>
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.25" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"/>
             </svg>
           </div>
           <div className="dashSidebarCardText _h1">Logout</div>
