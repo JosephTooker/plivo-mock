ff68c7855380789b91cd37e24aabf3fbd6287165
Victor <victorjosuepimentel21@gmail.com>
2022-09-25 23:24:35 -0400

    Clean up

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useChatContext } from 'stream-chat-react'
-import { UserList } from '.'
+import { UserList } from './'
 import { CloseCreateChannel } from '../assets/CloseCreateChannel'
 
 const ChannelNameInput = ({ channelName = '', setChannelName}) => {
